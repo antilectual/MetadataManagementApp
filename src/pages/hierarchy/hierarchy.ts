12bceda9eaec9786766101e2c0aee05fbe2f65e3
antilectual <matthewwjohnson@nevada.unr.edu>
2019-01-15 14:21:17 -0800

    Updated Hierarchy
    The hierarchy now allows for the names to be used rather than ontology categories

--- src/pages/hierarchy/hierarchy.ts
+++ src/pages/hierarchy/hierarchy.ts
@@ -15,10 +15,15 @@ import { ReadPage } from './read/read';
 })
 export class HierarchyPage {
 
+  // Ontology Items
   public items: any;
+  // Current header from ontology
   public hierarchyTop: any;
+  // URL for getting the specific data
   public subURI: string;
   public dataObject: any;
+  public currentDisplayPath: any;
+  public testObject = [{"Network":1,"Principal Investigator ID":"ae486fb0-749f-4e6e-98b4-eae9b5d7ff61","Name":"Walker Basin Hydroclimate","Alias":"ProtoNRDC","Institution Name":"University of Nevada, Reno","Original Funding Agency":"NSF","Grant Number String":"Grant No. 1230329","Started Date":"2012-06-01T00:00:00Z","Unique Identifier":"8f6fdbef-094e-42b0-96c8-70c2e801b889","Creation Date":"2016-03-31T17:21:25Z","Modification Date":"2016-03-31T17:21:25Z","Delete":false}];
 
   i = 0;
   constructor(public navCtrl: NavController, public http: HttpClient, public navParams: NavParams, public gvars: GlobalvarsProvider) {
@@ -30,6 +35,7 @@ export class HierarchyPage {
     {
       this.i = navParams.get('i');
     }
+    this.currentDisplayPath = navParams.get('name');
     this.getData(this.i);
   }
 
@@ -47,12 +53,25 @@ export class HierarchyPage {
     {
       let data: Observable<any> = this.http.get(local);
       data.subscribe(result => {
+        // Grab the json results from Ragnarok (Headers)
+        // i.e.
+        //  Site-Networks
+        //  Sites
+        //  Systems
+        //  Deployments
+        //  Components
+
         this.items = result;
+        // Get the current header item
         this.hierarchyTop = result[i];
+        // increases to next header item
         this.i = i + 1;
+        // Proper viewing name of header
         this.subURI = this.hierarchyTop.Plural;
+        // Create URL for the items from this header
         this.subURI = this.subURI.replace(/ +/g, "");
         this.subURI = dataRemote + this.subURI + ".svc/Get";
+
         this.getNextData();
       });
     }
@@ -68,18 +87,26 @@ export class HierarchyPage {
   }
 
   getNextData(){
-    console.log(this.subURI);
+    //DEBUG
+    //console.log(this.subURI);
     let data: Observable<any> = this.http.get(this.subURI);
     data.subscribe(result => {
       this.dataObject = result;
-      console.log(this.dataObject);
+      //DEBUG
+      //console.log(this.dataObject);
     });
   }
 
   push()
   {
-    let locali = {i:this.i};
-    this.navCtrl.push(HierarchyPage,locali);
+    let localValues = {i:this.i};
+    this.navCtrl.push(HierarchyPage,localValues);
+  }
+
+  push(item)
+  {
+    let localValues = {i:this.i, name:item.Name + " - "};
+    this.navCtrl.push(HierarchyPage,localValues);
   }
 
   viewCharacteristics()
