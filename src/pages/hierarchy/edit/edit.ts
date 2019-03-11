cb8a6d6e926b2b7c3a5d0a14004e2ca72a77ebb6
Matt <matthewwjohnson@nevada.unr.edu>
2019-03-10 21:58:42 -0700

    fix to includes

--- src/pages/hierarchy/edit/edit.ts
+++ src/pages/hierarchy/edit/edit.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HttpClient, RequestOptions } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { GlobalDataHandlerProvider } from '../../../providers/global-data-handler/global-data-handler';
 import { GlobalvarsProvider } from '../../../providers/globalvars/globalvars';
 
